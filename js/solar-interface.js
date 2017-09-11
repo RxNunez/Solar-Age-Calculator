5da1578c7c45652207e0ebf8a20b165a68628907
RxNunez <Nunez.ryan@gmail.com>
2017-09-11 00:06:05 -0700

    include interface solutions

--- js/solar-interface.js
+++ js/solar-interface.js
@@ -11,8 +11,13 @@ $(document).ready(function() {
     let venAge = solarCalculator.getVenus(age);
     let marAge = solarCalculator.getMars(age);
     let jupAge = solarCalculator.getJupiter(age);
+    let expectedYears = solarCalculator.timeLeft(world,userAge);
 
-      $('#solution').append("<li>" + element + "</li>");
+      $('#solution').append("<li>" + "Your Earth age is:" + userAge);
+      $('#solution').append("<li>" + "Your Mercury age is:" + merAge);
+      $('#solution').append("<li>" + "Your Venus age is:" + venAge);
+      $('#solution').append("<li>" + "Your Mars age is:" + marAge);
+      $('#solution').append("<li>" + "Your Jupiter age is:" + jupAge);
+      $('#solution').append("<li>" + expectedYears);
     });
   });
-});
