aa898d01395da37b5a2a170ac9d3912a8f9f9e5e
RxNunez <Nunez.ryan@gmail.com>
2017-09-10 23:45:10 -0700

    include interface variables

--- js/solar-interface.js
+++ js/solar-interface.js
@@ -3,10 +3,15 @@ import {SolarAge} from './../js/solar.js'
 $(document).ready(function() {
   $('#solar-form').submit(function(event) {
     event.preventDefault();
-    var bDay = $('#bDay').val();
-    var solarCalculator = new Calculator("hot pink");
-    var output = solarCalculator.solarAgeCalculator(bDay);
-    output.forEach(function(element) {
+    let bDay = $('#bDay').val();
+    let solarCalculator = new SolarAge();
+    let userAge = solarCalculator.getEarth(now, born);
+    let world = $('#world').val();
+    let merAge = solarCalculator.getMercury(age);
+    let venAge = solarCalculator.getVenus(age);
+    let marAge = solarCalculator.getMars(age);
+    let jupAge = solarCalculator.getJupiter(age);
+
       $('#solution').append("<li>" + element + "</li>");
     });
   });
